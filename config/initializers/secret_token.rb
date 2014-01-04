127f0aa78960e17181085ec088c2629f1d429e44
Pavel Gorlov <pavel0788@yandex.ru>
2014-01-04 15:51:29 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Redirectme::Application.config.secret_key_base = '59328f38aca50c966bbc380c5511ab4eceba5d230f1477538bd668731af225761a1c7ec180b07ec9f85f9694bf7b77a43945d38aebaf7647825d44c67601fb92'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Redirectme::Application.config.session_store :cookie_store, key: '_redirectme_session'
