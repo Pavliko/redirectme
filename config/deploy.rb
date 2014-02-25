8f292a70cd1b278238913da78e483f53262f5188
Pavel Gorlov <pavel0788@yandex.ru>
2014-02-26 03:08:32 +0700

    ss

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :sidekiq_processes, 1
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log']
+set :shared_paths, ['config/database.yml', 'log', 'tmp']
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
@@ -91,7 +91,7 @@ task :deploy => :environment do
 
     to :launch do
       invoke 'sidekiq:restart'
-      queue "touch #{deploy_to}/tmp/restart.txt"
+      queue "touch #{app_path}/tmp/restart.txt"
     end
   end
 end
